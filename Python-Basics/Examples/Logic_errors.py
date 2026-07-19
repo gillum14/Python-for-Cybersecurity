2935aabae2a13338cb0cce6eeed80edc40c04a81
Caitlin Gillum <caitlingillum93@gmail.com>
2026-07-19 17:56:45 -0500

    Create Logic_errors.py

---
+++ Python-Basics/Examples/Logic_errors.py
@@ -0,0 +1,16 @@
+system = "OS 3"
+
+patch_schedule = [
+    "March",
+    "April",
+    "May"
+]
+
+if system == "OS 1":
+    print(patch_schedule[0])
+
+elif system == "OS 2":
+    print(patch_schedule[1])
+
+elif system == "OS 3":
+    print(patch_schedule[2])
